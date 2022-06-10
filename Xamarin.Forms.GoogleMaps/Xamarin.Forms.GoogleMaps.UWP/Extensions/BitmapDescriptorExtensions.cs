d2cf68a86321126c5c19a33e14d66f91f6806788
Edward Miller <symbiogenesis@outlook.com>
2022-06-10 17:22:53 -0500

    remove unused usings

--- Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps.UWP/Extensions/BitmapDescriptorExtensions.cs
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps.UWP/Extensions/BitmapDescriptorExtensions.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Windows.UI.Xaml.Controls.Maps;
 using Windows.UI.Xaml.Media.Imaging;
-using Xamarin.Forms.GoogleMaps;
 
 namespace Xamarin.Forms.GoogleMaps.Extensions.UWP
 {
@@ -20,7 +14,7 @@ namespace Xamarin.Forms.GoogleMaps.Extensions.UWP
                     //Intercepted in Pushpin.cs to render using Xamarin.Forms contentTemplate
                     return new BitmapImage();
                 case BitmapDescriptorType.Bundle:
-                    return new BitmapImage(new Uri(String.Format("ms-appx:///{0}", self.BundleName)));
+                    return new BitmapImage(new Uri(string.Format("ms-appx:///{0}", self.BundleName)));
                 case BitmapDescriptorType.Stream:
                     var bitmap = new BitmapImage();
                     var memoryStream = new MemoryStream();
